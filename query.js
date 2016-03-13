8efe01a8c928e53aaaa0215e5e98c9f6c8959157
Matthew Hoselton <mtthwhsltn@gmail.com>
2016-03-12 21:24:35 -0800

    query string info

---
+++ query.js
@@ -0,0 +1,23 @@
+{ 
+  "Normalized": false,
+  "NumberOfDays": 365,
+  "DataPeriod": "Day",
+  "Elements": 
+    [ 
+      { 
+        "Symbol":"AAPL", 
+        "Type":"price",
+        "Params":["c"]
+      },
+      { 
+        "Symbol":"MSFT",
+        "Type":"price",
+        "Params":["c"]
+      } 
+    ] 
+}
+// API Documentation
+// http://dev.markitondemand.com/MODApis/#doc_interactivechart
+
+// API endpoint for interactive chart
+// http://dev.markitondemand.com/MODApis/Api/v2/InteractiveChart/json?parameters=
